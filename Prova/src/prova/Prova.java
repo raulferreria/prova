a181a034ec669b3646a409166c1c476286330ebd
raul vallez <120009610+raulferreria@users.noreply.github.com>
2022-12-20 15:29:24 +0100

    /*  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template  */ package prova;
    /**
     *
     * @author rvallez2
     */
    public class Prova {
    
        /**
         * @param args the command line arguments
         */
        public static void main(String[] args) {
            // TODO code application logic here
            System.out.println("hola");
            System.out.println("hola");
            System.out.println("hola");
        }
    
    }

--- Prova/src/prova/Prova.java
+++ Prova/src/prova/Prova.java
@@ -16,6 +16,8 @@ public class Prova {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola");
+        System.out.println("hola");
+        System.out.println("hola");
     }
     
 }
